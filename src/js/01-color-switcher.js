51b258baf6c85c54279d3fa7d02525da0a1f90ae
Of-Nameless <79377789+Of-Nameless@users.noreply.github.com>
2023-02-03 18:23:36 +0100

    color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const button = document.querySelectorAll('button');
+let intervalColor = undefined;
 
+button[0].addEventListener('click', startBtn);
+button[1].addEventListener('click', stopBtn);
+
+function startBtn() {
+    button[0].disabled = true;
+    button[1].disabled = false;
+    intervalColor = setInterval(() => {
+        document.body.style.background = getRandomHexColor()
+    }, 1000);
+};
+
+function stopBtn(e) {
+    button[0].disabled = false;
+    button[1].disabled = true;
+    clearInterval(intervalColor)
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
